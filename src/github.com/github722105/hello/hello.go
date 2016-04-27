5e384980104c9b2c36de46a4550287d64d8bd9b4
fderamos <ferdinand.delumban.deramos@gmail.com>
2016-04-27 04:20:06 +0000

    initial commit

---
+++ src/github.com/github722105/hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+    "github.com/github722105/stringutil"
+)
+
+func main() {
+    fmt.Printf("hello, world\n")
+    fmt.Printf(stringutil.Reverse("hello, world")+"\n")
+}
